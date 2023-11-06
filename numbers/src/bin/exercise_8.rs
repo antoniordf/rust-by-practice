a316936eba493d8ffb43e205e905b777ea0a3424
Antonio Furtado <antoniordf@hotmail.com>
2023-11-06 16:31:43 +0000

    exercises

---
+++ numbers/src/bin/exercise_8.rs
@@ -0,0 +1,6 @@
+// 🌟🌟 Make it work in two distinct ways
+fn main() {
+    assert!(0.1_f32 + 0.2_f32 == 0.3_f32); // could have used the as keyword: 0.1 as f32 + 0.2 as f32 == 0.3 as f32
+
+    println!("Success!");
+}
