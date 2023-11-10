ae6a473207d20e2dc248524917e6db1a997abef9
Antonio Furtado <antoniordf@hotmail.com>
2023-11-10 18:22:49 +0000

    exercises

---
+++ compound-types-string/src/bin/exercise_12.rs
@@ -0,0 +1,21 @@
+// Operate on UTF8 string
+// 🌟
+fn main() {
+    // Fill the blank to print each char in "你好，世界"
+    for c in "你好，世界".chars() {
+        println!("{}", c)
+    }
+}
+
+// utf8_slice
+// You can use utf8_slice to slice UTF8 string, it can index chars instead of bytes.
+
+// Example
+
+// use utf8_slice;
+// fn main() {
+//     let s = "The 🚀 goes to the 🌑!";
+
+//     let rocket = utf8_slice::slice(s, 4, 5);
+//     // Will equal "🚀"
+}
