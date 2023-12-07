f48d932c07d006e089e8234e2e4da84e55956747
Antonio Furtado <antoniordf@hotmail.com>
2023-12-07 00:08:47 +0000

    exercises

---
+++ compound-types-enum/src/bin/exercise_5.rs
@@ -0,0 +1,23 @@
+// 🌟🌟 Since there is no null in Rust, we have to use enum Option<T> to deal with the cases when the value is absent.
+// Fill in the blank to make the `println` work.
+// Also add some code to prevent the `panic` from running.
+fn main() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    if let Some(n) = six {
+        println!("{}", n);
+
+        println!("Success!");
+    }
+
+    panic!("NEVER LET THIS RUN!");
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
